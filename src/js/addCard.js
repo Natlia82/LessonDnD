24ff9a663cc17ab795c45780d72afe84ae1013df
Natalia <nata@host>
2021-12-23 12:52:49 +0500

    добавление нового элемента

--- src/js/addCard.js
+++ src/js/addCard.js
@@ -1,49 +1,41 @@
 /******добавление элемента*****/
-
-/*
-const but = document.querySelector('.add');
-but.addEventListener("click", cliki);
-
 function cliki() {
     const li = document.createElement('li');
+    const text = document.getElementById('text').value;
     li.className = 'tasks__item';
     li.draggable = true;
-    li.textContent = 'новый элемент';
+    li.textContent = text;
 
     let elem = document.querySelector('.tasks__list');
-    console.log(elem);
     elem.appendChild(li);
+    togleTextarea();
+    removeAddButton();
 }
-*/
+
 function removeAddButton() {
     const buttonAdd = document.querySelector('.add_card');
     buttonAdd.classList.toggle('displayNone');
     const AddCard = document.querySelector('.add');
     AddCard.classList.toggle('displayNone');
-    //  buttonAdd.classList.add('add');
-    //  buttonAdd.classList.remove('add_card');
+}
+
+function togleTextarea() {
+    const textCuntentNew = document.getElementById("textCuntentNew");
+    document.getElementById('text').value = "";
+    textCuntentNew.classList.toggle("displayNone");
 }
 
 function addCard() {
-    const li = document.createElement('li');
-    li.className = 'tasks__item';
-    li.className = 'new_tasks__item';
-    li.draggable = true;
-    //  li.textContent = 'новый элемент';
-
-    /*создаем элемент для ввода текста*/
-    const input = document.createElement("textarea");
-    input.maxLength = "100";
-    input.classList.add('textNew')
-
-    li.appendChild(input);
-    //добавление созданного элемента в контент
-    let elem = document.querySelector('.tasks__list');
-    //  console.log(elem);
-    elem.appendChild(li);
+    togleTextarea();
     removeAddButton();
 }
+
 const butAdd = document.querySelector('.add_card');
 butAdd.addEventListener('click', () => {
     addCard();
+});
+
+const but = document.querySelector('.add');
+but.addEventListener("click", (e) => {
+    cliki();
 });
