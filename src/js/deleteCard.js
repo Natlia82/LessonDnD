62fca16f235bd0794a2d42b8ed42387714c78bce
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2021-12-30 12:43:33 +0500

    пробелы

--- src/js/deleteCard.js
+++ src/js/deleteCard.js
@@ -2,22 +2,20 @@ import InsertLocalStore from './localStore';
 
 const cursorCard = document.querySelector('.contener');
 cursorCard.addEventListener('mouseover', (e) => {
-    if (e.target.tagName === 'LI') {
-        e.target.children[0].classList.remove('displayNone');
-    }
+  if (e.target.tagName === 'LI') {
+      e.target.children[0].classList.remove('displayNone');
+  }
 });
 cursorCard.addEventListener('mouseout', (e) => {
-    if (e.target.tagName === 'LI') {
-        e.target.children[0].classList.add('displayNone');
-    }
+  if (e.target.tagName === 'LI') {
+      e.target.children[0].classList.add('displayNone');
+  }
 });
 
 cursorCard.addEventListener('click', (e) => {
-    if (e.target.classList.contains('deleteLi')) {
-        // e.target.parentNode.remove()
-        const n = e.target.parentNode.parentNode.parentNode.getAttribute('data-sevtion');
-        //console.log(n);
-        e.target.parentNode.remove();
-        InsertLocalStore(n);
-    }
+  if (e.target.classList.contains('deleteLi')) {
+    const n = e.target.parentNode.parentNode.parentNode.getAttribute('data-sevtion');
+    e.target.parentNode.remove();
+    InsertLocalStore(n);
+  }
 });
