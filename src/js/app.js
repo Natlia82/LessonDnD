74bff7f86a4ff63db78a53d9b0e8a7d35a7057d3
Natalia <nata@host>
2022-01-31 14:47:57 +0500

    перетаск-е из блока в блок

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,21 @@
-const elementTasks = document.querySelectorAll('.tasks__list');
-for (let i = 0; i < elementTasks.length; i += 1) {
-  const tasksListElement = elementTasks[i];
-  const taskElements = tasksListElement.querySelectorAll('.tasks__item');
+const tasksContener = document.querySelector('.contener');
+const tasksListElement = document.querySelectorAll('.tasks__list');
+const taskElements = tasksContener.querySelectorAll('.tasks__item');
 
-  for (let j = 0; j < taskElements.length; j += 1) {
-    taskElements[j].draggable = true;
-  }
+for (let i = 0; i < taskElements.length; i++) {
+    taskElements[i].draggable = true;
+}
 
-  tasksListElement.addEventListener('dragstart', (evt) => {
+
+tasksContener.addEventListener('dragstart', (evt) => {
     evt.target.classList.add('selected');
-  });
+})
 
-  tasksListElement.addEventListener('dragend', (evt) => {
+tasksContener.addEventListener('dragend', (evt) => {
     evt.target.classList.remove('selected');
-  });
+});
 
-  const getNextElement = (cursorPosition, currentElement) => {
+const getNextElement = (cursorPosition, currentElement) => {
     // Получаем объект с размерами и координатами
     const currentElementCoord = currentElement.getBoundingClientRect();
     // Находим вертикальную координату центра текущего элемента
@@ -23,24 +23,25 @@ for (let i = 0; i < elementTasks.length; i += 1) {
 
     // Если курсор выше центра элемента, возвращаем текущий элемент
     // В ином случае — следующий DOM-элемент
-    const nextElement = (cursorPosition < currentElementCenter)
-      ? currentElement
-      : currentElement.nextElementSibling;
+    const nextElement = (cursorPosition < currentElementCenter) ?
+        currentElement :
+        currentElement.nextElementSibling;
 
     return nextElement;
-  };
+};
 
-  tasksListElement.addEventListener('dragover', (evt) => {
+tasksContener.addEventListener('dragover', (evt) => {
     evt.preventDefault();
 
-    const activeElement = tasksListElement.querySelector('.selected');
+    const activeElement = tasksContener.querySelector('.selected');
     const currentElement = evt.target;
-    const isMoveable = activeElement !== currentElement
-            && currentElement.classList.contains('tasks__item');
+    const isMoveable = activeElement !== currentElement &&
+        currentElement.classList.contains('tasks__item');
 
     if (!isMoveable) {
-      return;
+        return;
     }
+
 
     // evt.clientY — вертикальная координата курсора в момент,
     // когда сработало событие
@@ -48,14 +49,15 @@ for (let i = 0; i < elementTasks.length; i += 1) {
 
     // Проверяем, нужно ли менять элементы местами
     if (
-      nextElement
-            && (activeElement === nextElement.previousElementSibling
-                || activeElement === nextElement)
+        nextElement &&
+        activeElement === nextElement.previousElementSibling ||
+        activeElement === nextElement
     ) {
-      // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
-      return;
+        // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
+        return;
     }
 
-    tasksListElement.insertBefore(activeElement, nextElement);
-  });
-}
+    currentElement.closest('.tasks__list').insertBefore(activeElement, nextElement);
+    //  tasksListElement[1].insertBefore(activeElement, nextElement);
+
+});
